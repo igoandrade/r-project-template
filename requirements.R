da19755f5f2b8f5afd8b6d97680e864360f17f66
Igo da Costa Andrade <igoandrade.314@gmail.com>
2022-01-01 19:17:54 +0000

    Update files

--- requirements.R
+++
@@ -1,8 +0,0 @@
-# Package names
-packages <- c("ggplot2", "readxl", "dplyr", "tidyr", "caret", "data.table", "mlr3", "knitr", "plotly", "shiny", "tidyr", "car", "tidyverse", "tidyquant", "xgboost", "markdown", "MASS",  "magrittr")
-
-# Install packages not yet installed
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
-}
